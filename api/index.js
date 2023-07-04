4fcfb17ddf5c299e6d68020b45163479f5764d1d
Akhil Sharma <akhilsharma6721@gmail.com>
2023-07-04 08:09:52 +0530

    deployment changes

--- api/index.js
+++ api/index.js
@@ -12,6 +12,16 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 app.use('/', routes);
 
+const path = require('path');
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server connected on port ${port}`);
